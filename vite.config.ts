519ca17bcc8374b368491b67c94ae71af192565f
Matvei <105492881+matb3l@users.noreply.github.com>
2024-11-01 20:18:38 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
-import path from 'path';
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      'pages': path.resolve(__dirname, './src/pages'),
-    }
-  }
+      pages: path.resolve(__dirname, './src/pages'),
+    },
+  },
 })
