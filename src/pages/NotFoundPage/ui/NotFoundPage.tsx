e6875400248e7ca69a82c827ef29a1b0c3151006
Матвей Белавин <m.belavin@21-school.ru>
2025-06-22 15:04:44 +0300

    gitlab ci

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,24 +1,18 @@
 import { Box, Typography } from '@mui/material'
 import { Button } from '@ui/Button/Button.tsx'
 import { useTranslation } from 'react-i18next'
+import styles from './NotFoundPage.module.css'
 
 export const NotFoundPage = () => {
   const { t } = useTranslation('NotFound')
   return (
     <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        height: '100%',
-        backgroundColor: '#f44336',
-      }}
+      className={styles.notFoundPage}
     >
-      <Typography variant='h1' style={{ color: 'white' }}>
+      <Typography variant='h1' color='textPrimary'>
         404
       </Typography>
-      <Typography variant='h6' style={{ color: 'white' }}>
+      <Typography variant='h6' color="textPrimary">
         {t('Страница не найдена')}
       </Typography>
       <Button variant='contained'>{t('Вернуться домой')}</Button>
