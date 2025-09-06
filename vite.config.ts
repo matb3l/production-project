ee142050581f322443123f3d2c7203517b3244ca
matvei <m.belavin05@yandex.ru>
2025-09-06 16:42:32 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/production-project",
   test: {
     globals: true,
     environment: 'jsdom',
